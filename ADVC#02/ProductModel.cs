69029b0e8ad0695d6b7fb01b0c67b9029406518a
KerolosAshraf29 <karam.kerolos222@gmail.com>
2026-08-31 23:09:36 +0300

    Answers

---
+++ ADVC#02/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADVC_02
+{
+    internal class Product
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // "Electronics" , "Clothing" , "Food" , "Books"
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
